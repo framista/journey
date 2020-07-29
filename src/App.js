dab24adb25d269157ebd9290b7e92c6b8b582bdd
framista <adajab1@gmail.com>
2020-07-29 22:28:52 +0200

    Create component for situation, when there is nothing to show

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'normalize.css';
 import './scss/base/App.scss';
 import JourneyTile from './components/JourneyTile';
+import NoJourney from './components/NoJourney';
 
 const journeysData = [
   {
@@ -14,6 +15,54 @@ const journeysData = [
   },
   {
     id: 2,
+    localization: 'Spain',
+    date: '02.06.2011',
+    image:
+      'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['sister'],
+  },
+  {
+    id: 11,
+    localization: 'Egypt',
+    date: '12.06.2009',
+    image:
+      'https://images.unsplash.com/photo-1566192091743-5966a6079984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['father'],
+  },
+  {
+    id: 22,
+    localization: 'Spain',
+    date: '02.06.2011',
+    image:
+      'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['sister'],
+  },
+  {
+    id: 111,
+    localization: 'Egypt',
+    date: '12.06.2009',
+    image:
+      'https://images.unsplash.com/photo-1566192091743-5966a6079984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['father'],
+  },
+  {
+    id: 222,
+    localization: 'Spain',
+    date: '02.06.2011',
+    image:
+      'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['sister'],
+  },
+  {
+    id: 1111,
+    localization: 'Egypt',
+    date: '12.06.2009',
+    image:
+      'https://images.unsplash.com/photo-1566192091743-5966a6079984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['father'],
+  },
+  {
+    id: 2222,
     localization: 'Spain',
     date: '02.06.2011',
     image:
@@ -38,16 +87,13 @@ function App() {
             {journeys.map((journey) => (
             <JourneyTile
               journey={journey}
-              key={journey.date}
+              key={journey.id}
               deleteJourney={handleDeleteJourney}
             />
             ))}
           </div>
         ) : (
-          <div className="no-journey">
-            <h2 className="no-journey__h2">You don't have any journeys</h2>
-            <button className="no-journey__btn">Add journey</button>
-          </div>
+         <NoJourney />
         )}
       </section>
     </main>
---
+++ src/components/NoJourney.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoJourney() {
+  return (
+    <div className="no-journey">
+      <h2 className="no-journey__h2">You don't have any journeys</h2>
+      <button className="no-journey__btn">Add journey</button>
+    </div>
+  );
+}
+
+export default NoJourney;
