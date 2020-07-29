aea17156fe2b80f5b497a83dfc367c3a21614b80
framista <adajab1@gmail.com>
2020-07-29 22:04:27 +0200

    Delete single journey

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'normalize.css';
 import './scss/base/App.scss';
 import JourneyTile from './components/JourneyTile';
 
+const journeysData = [
+  {
+    id: 1,
+    localization: 'Egypt',
+    date: '12.06.2009',
+    image:
+      'https://images.unsplash.com/photo-1566192091743-5966a6079984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['father'],
+  },
+  {
+    id: 2,
+    localization: 'Spain',
+    date: '02.06.2011',
+    image:
+      'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    companions: ['sister'],
+  },
+];
+
 function App() {
-  const journeys = [
-    {
-      localization: 'Egypt',
-      date: '12.06.2009',
-      image:
-        'https://images.unsplash.com/photo-1566192091743-5966a6079984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-      companions: ['father'],
-    },
-    {
-      localization: 'Spain',
-      date: '02.06.2011',
-      image:
-        'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-      companions: ['sister'],
-    },
-  ];
+
+  const [journeys, setJourneys] = useState(journeysData);
+
+  const handleDeleteJourney = (id) => {
+    const journeysFiltered = journeys.filter(journey => journey.id !== id ); 
+    setJourneys(journeysFiltered)
+  }
 
   return (
     <main className="container">
       <section className="center-container">
         <div className="journey-container">
           {journeys.map((journey) => (
-            <JourneyTile journey={journey} />
+            <JourneyTile journey={journey} key={journey.date} deleteJourney={handleDeleteJourney}/>
           ))}
         </div>
       </section>
--- src/components/JourneyTile.js
+++ src/components/JourneyTile.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { TiDelete } from 'react-icons/ti';
 
 function JourneyTile(props) {
-  const { localization, date, image, companions } = props.journey;
+  const {deleteJourney} = props;
+  const { id, localization, date, image, companions } = props.journey;
   return (
-  <div className="journey">
-      <img className="journey__img" src={image} alt={localization}/>
+    <div className="journey" onClick={(e) => console.log(localization + date)}>
+      <img className="journey__img" src={image} alt={localization} />
       <div className="journey__description">
-          <h3 className="journey__h3">{localization}</h3>
-          <p className="journey__p">{date}</p>
+        <h3 className="journey__h3">{localization}</h3>
+        <p className="journey__p">{date}</p>
       </div>
-  </div>);
+      <TiDelete className="journey__icon" onClick={(e) => {
+       e.stopPropagation();
+       deleteJourney(id);
+      }}/>
+    </div>
+  );
 }
 
 export default JourneyTile;
