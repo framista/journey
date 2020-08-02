b99a97ed9b147fdc5a82bac5ff48844f3d995799
framista <adajab1@gmail.com>
2020-08-02 23:58:55 +0200

    Create layout for not-found-page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function NotFoundPage() {
-    return (
-        <div>
-            Not found page
+  return (
+    <main className="container">
+      <section className="no-page">
+        <div className="no-page__info">
+          <h2 className="no-page__title">Oops, page not found</h2>
+          <p className="no-page__description">We are very sorry for the inconvenience. It looks like you're trying to access a page that has been deleted or never even existed.</p>
+          <button className="no-page__btn">Back to homepage</button>
         </div>
-    )
+        <div className="no-page__picture">
+          <img src="https://images.unsplash.com/photo-1549747862-084be795471b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="desert-island"/>
+          <p className="no-page__error">404</p>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
