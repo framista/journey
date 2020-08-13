41424ded9b69683fc7dcc1c4831ac6f3023de2d7
framista <adajab1@gmail.com>
2020-08-13 20:26:13 +0200

    Updates

---
+++ precache-manifest.1e98f6b8239e6745d7782225705f056a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9428b1aaea63c5008da11248edee8c18",
+    "url": "/journey/index.html"
+  },
+  {
+    "revision": "8ebc0cf51e6bcc7d0eda",
+    "url": "/journey/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7dcb46542b460984975a",
+    "url": "/journey/static/css/main.a0a857fa.chunk.css"
+  },
+  {
+    "revision": "8ebc0cf51e6bcc7d0eda",
+    "url": "/journey/static/js/2.7a07a5ed.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/journey/static/js/2.7a07a5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcb46542b460984975a",
+    "url": "/journey/static/js/main.941ab34f.chunk.js"
+  },
+  {
+    "revision": "c913ea418c6b78309d4c",
+    "url": "/journey/static/js/runtime-main.d85ad578.js"
+  }
+]);
