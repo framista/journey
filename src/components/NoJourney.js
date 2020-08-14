f12b5c68ef76d6f6f4a6b2fa8bcdfc929588b5f2
framista <adajab1@gmail.com>
2020-08-14 18:59:23 +0200

    Add link to button, when there is no data

--- src/components/NoJourney.js
+++ src/components/NoJourney.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NoJourney() {
   return (
     <div className="no-journey">
       <h2 className="no-journey__h2">You don't have any journeys</h2>
-      <button className="no-journey__btn">Add journey</button>
+      <Link to="/create" className="no-journey__btn">Add journey</Link>
     </div>
   );
 }
