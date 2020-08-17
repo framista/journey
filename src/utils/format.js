c934d9661439831d82e4fc837a30061f0e14ed38
framista <adajab1@gmail.com>
2020-08-17 19:30:22 +0200

    Add function to format time how ago is and test this function

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,3 +2,28 @@ const formatSingleNumber = (number) => (number < 10 ? '0' + number : number);
 
 export const formatDate = ({ day, month, year }) =>
   `${formatSingleNumber(day)}.${formatSingleNumber(month)}.${year}`;
+
+const formatter = new Intl.RelativeTimeFormat('en-us', {
+  numeric: 'auto',
+});
+
+const DIVISIONS = [
+  { amount: 60, name: 'seconds' },
+  { amount: 60, name: 'minutes' },
+  { amount: 24, name: 'hours' },
+  { amount: 7, name: 'days' },
+  { amount: 4.34524, name: 'weeks' },
+  { amount: 12, name: 'months' },
+  { amount: Number.POSITIVE_INFINITY, name: 'years' },
+];
+
+export const formatTimeAgo = (date) => {
+  let duration = (date - new Date()) / 1000;
+  for (let i = 0; i <= DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name);
+    }
+    duration /= division.amount;
+  }
+};
--- src/utils/format.test.js
+++ src/utils/format.test.js
@@ -1,10 +1,35 @@
-import { formatDate } from './format';
+import { formatDate, formatTimeAgo } from './format';
+import { set, reset } from 'mockdate';
 
 describe('format date', () => {
   it('should return formatted date', () => {
     const expectedData = '12.06.2015';
     const data = { day: 12, month: 6, year: 2015 };
     const result = formatDate(data);
+    expect(result).toBe(expectedData);
+  });
+});
+
+describe('format time ago', () => {
+  const date = '2020-04-04T18:09:12.451Z';
+  beforeEach(() => {
+    set(date);
+  });
+  afterEach(() => {
+    reset();
+  });
+  it('should return 5 minutes ago', () => {
+    const expectedData = '5 minutes ago';
+    const currentDate = new Date();
+    const data = new Date().setMinutes(currentDate.getMinutes() - 5);
+    const result = formatTimeAgo(data);
+    expect(result).toBe(expectedData);
+  });
+  it('should return 3 years', () => {
+    const expectedData = '3 years ago';
+    const currentDate = new Date();
+    const data = new Date().setFullYear(currentDate.getFullYear() - 3);
+    const result = formatTimeAgo(data);
     expect(result).toBe(expectedData);
   });
 });
