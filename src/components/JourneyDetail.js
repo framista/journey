8fcfc7d03bf43d1612f0da7d585f4a13bb607e97
framista <adajab1@gmail.com>
2020-08-18 21:55:08 +0200

    Create component to show details about journey

---
+++ src/components/JourneyDetail.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+function JourneyDetail(props) {
+  const { journey, setBlockClicking, setDetail } = props;
+  const {
+    id,
+    location,
+    startDate,
+    description,
+    travellingCompanion,
+    imageFile,
+  } = journey;
+
+  const closeModal = (e) => {
+    e.stopPropagation();
+    setDetail(false);
+    setBlockClicking(false);
+  };
+
+  const handleEdit = (e) => {
+    console.log(id);
+  };
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="background"></div>
+      <div className="journey-detail">
+        <div className="journey-detail__title">
+          <h5>{location}</h5>
+        </div>
+        <div className="journey-detail__body">
+          <div className="journey-detail__image">
+            {imageFile ? (
+              <img src={imageFile} alt="journey-img" />
+            ) : (
+              <img
+                src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                alt="journey-img"
+              />
+            )}
+          </div>
+          <p className="journey-detail__location">
+            Your journey in&nbsp;
+            <span>{location}</span> started at&nbsp;
+            <span>
+              {startDate.day}.{startDate.month}.{startDate.year}
+            </span>
+            .
+          </p>
+          {travellingCompanion.length > 0 && (
+            <>
+              <p className="journey-detail__travelling">
+                You spent your vacation with:
+              </p>
+              <ul className="journey-detail__list">
+                {travellingCompanion.map((companion, index) => (
+                  <li key={index} className="journey-detail__list--item">
+                    {companion.trim()}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+          {description && (
+            <p className="journey-detail__description">{description}</p>
+          )}
+        </div>
+        <div className="journey-detail__footer">
+          <button
+            className="journey-detail__button journey-detail__button--edit"
+            onClick={(e) => handleEdit(e, id)}
+          >
+            edit
+          </button>
+          <button
+            className="journey-detail__button journey-detail__button--cancel"
+            onClick={closeModal}
+          >
+            close
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+}
+
+JourneyDetail.propTypes = {
+  journey: PropTypes.object.isRequired,
+  setBlockClicking: PropTypes.func.isRequired,
+  setDetail: PropTypes.func.isRequired,
+};
+
+export default JourneyDetail;
--- src/components/JourneyTile.js
+++ src/components/JourneyTile.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import JourneyDeleteModal from './JourneyDeleteModal';
 import Avatar from 'react-avatar';
 import { formatTimeAgo } from '../utils/format';
+import JourneyDetail from './JourneyDetail';
 
 function JourneyTile(props) {
   const { journey, deleteJourney, setBlockClicking } = props;
   const { location, startDate, imageFile } = journey;
   const [deleteModal, setDeleteModal] = useState(false);
+  const [detail, setDetail] = useState(false);
 
   const handleDelete = (e) => {
     e.stopPropagation();
@@ -16,35 +18,44 @@ function JourneyTile(props) {
   };
 
   const handleShow = () => {
-    console.log(location + startDate);
+    console.log(location, startDate);
+    setDetail(true);
   };
 
-  console.log(startDate);
   return (
-    <div className="journey" onClick={handleShow}>
-      {imageFile ? (
-        <img className="journey__img" src={imageFile} alt="" />
-      ) : (
-        <Avatar name={location.split(' ')[0]} size={60} round="50%" />
-      )}
-      <div className="journey__description">
-        <h3 className="journey__h3">{location}</h3>
-        <p className="journey__p">
-          {formatTimeAgo(
-            new Date(startDate.year, startDate.month - 1, startDate.day)
-          )}
-        </p>
+    <Fragment>
+      <div className="journey" onClick={handleShow}>
+        {imageFile ? (
+          <img className="journey__img" src={imageFile} alt="" />
+        ) : (
+          <Avatar name={location.split(' ')[0]} size={60} round="50%" />
+        )}
+        <div className="journey__description">
+          <h3 className="journey__h3">{location}</h3>
+          <p className="journey__p">
+            {formatTimeAgo(
+              new Date(startDate.year, startDate.month - 1, startDate.day, 12)
+            )}
+          </p>
+        </div>
+        <TiDelete className="journey__icon" onClick={(e) => handleDelete(e)} />
+        {deleteModal && (
+          <JourneyDeleteModal
+            journey={journey}
+            deleteJourney={deleteJourney}
+            setDeleteModal={setDeleteModal}
+            setBlockClicking={setBlockClicking}
+          />
+        )}
       </div>
-      <TiDelete className="journey__icon" onClick={(e) => handleDelete(e)} />
-      {deleteModal && (
-        <JourneyDeleteModal
+      {detail && (
+        <JourneyDetail
           journey={journey}
-          deleteJourney={deleteJourney}
-          setDeleteModal={setDeleteModal}
+          setDetail={setDetail}
           setBlockClicking={setBlockClicking}
         />
       )}
-    </div>
+    </Fragment>
   );
 }
 
