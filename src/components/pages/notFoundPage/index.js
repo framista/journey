34ca76ee5f2a4957e4900379c56299a88eee08a9
framista <adajab1@gmail.com>
2020-08-19 19:30:04 +0200

    Change structure of project and add export withRouter for not found page

---
+++ src/components/pages/notFoundPage/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+function NotFoundPage(props) {
+  return (
+    <main className="container">
+      <section className="no-page">
+        <div className="no-page__info">
+          <h2 className="no-page__title">Oops, page not found</h2>
+          <p className="no-page__description">
+            We are very sorry for the inconvenience. It looks like you're trying
+            to access a page that has been deleted or never even existed.
+          </p>
+          <button
+            className="no-page__btn"
+            onClick={() => props.history.push('/')}
+          >
+            Back to homepage
+          </button>
+        </div>
+        <div className="no-page__picture">
+          <img
+            src="https://images.unsplash.com/photo-1549747862-084be795471b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt="desert-island"
+          />
+          <p className="no-page__error">404</p>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default withRouter(NotFoundPage);
